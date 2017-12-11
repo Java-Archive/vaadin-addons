fcd1c7d4a5875f4902f7993aa48998a42c5a2530
Sven Ruppert <sven.ruppert@gmail.com>
2017-12-11 13:49:59 +0100

    ServletContainerExtension

---
+++ modules/testbench/src/main/java/org/rapidpm/vaadin/addons/testbench/junit5/extensions/container/ServletContainerExtension.java
@@ -0,0 +1,41 @@
+package org.rapidpm.vaadin.addons.testbench.junit5.extensions.container;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.rapidpm.dependencies.core.logger.HasLogger;
+import org.rapidpm.frp.functions.CheckedConsumer;
+import org.rapidpm.frp.functions.CheckedExecutor;
+import org.rapidpm.microservice.Main;
+
+import static org.rapidpm.vaadin.addons.testbench.BrowserDriverFunctions.localeIP;
+import static org.rapidpm.vaadin.addons.testbench.junit5.VaadinPageObject.KEY_VAADIN_SERVER_IP;
+
+/**
+ *
+ */
+public class ServletContainerExtension implements BeforeAllCallback, BeforeEachCallback, AfterEachCallback, HasLogger {
+
+  @Override
+  public void beforeEach(ExtensionContext context) throws Exception {
+    //TODO hook in with Servletcontainer
+    logger().info("ServletContainerExtension - beforeEach");
+    Main.deploy();
+  }
+
+
+  @Override
+  public void afterEach(ExtensionContext context) throws Exception {
+    //TODO hook in with Servletcontainer
+    logger().info("ServletContainerExtension - afterEach");
+    Main.stop();
+  }
+
+  @Override
+  public void beforeAll(ExtensionContext context) throws Exception {
+    final String userVaadinServerIP = localeIP().get();
+    logger().info(KEY_VAADIN_SERVER_IP + " ServletContainerExtension - will be -> " + userVaadinServerIP);
+    System.setProperty(KEY_VAADIN_SERVER_IP, userVaadinServerIP);
+  }
+}
