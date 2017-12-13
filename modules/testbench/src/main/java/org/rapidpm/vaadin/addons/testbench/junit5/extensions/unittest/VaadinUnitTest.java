6a3abbe31a2c3f0c3b9fd5693be01fcd3979f7bf
Sven Ruppert <sven.ruppert@gmail.com>
2017-12-13 16:11:20 +0100

    unit - testing with Selenium/ localebrowser

---
+++ modules/testbench/src/main/java/org/rapidpm/vaadin/addons/testbench/junit5/extensions/unittest/VaadinUnitTest.java
@@ -0,0 +1,24 @@
+package org.rapidpm.vaadin.addons.testbench.junit5.extensions.unittest;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.rapidpm.vaadin.addons.testbench.junit5.extensions.container.ServletContainerExtension;
+import org.rapidpm.vaadin.addons.testbench.junit5.pageobject.PageObjectExtension;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ */
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(ServletContainerExtension.class)
+@ExtendWith(WebdriverExtension.class)
+@ExtendWith(PageObjectExtension.class)
+public @interface VaadinUnitTest {
+}
+
+
---
+++ modules/testbench/src/main/java/org/rapidpm/vaadin/addons/testbench/junit5/extensions/unittest/WebdriverExtension.java
@@ -0,0 +1,34 @@
+package org.rapidpm.vaadin.addons.testbench.junit5.extensions.unittest;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.rapidpm.dependencies.core.logger.HasLogger;
+
+import static org.rapidpm.vaadin.addons.testbench.BrowserDriverFunctions.unittestingWebDriverInstance;
+import static org.rapidpm.vaadin.addons.testbench.junit5.extensions.ExtensionFunctions.*;
+
+/**
+ *
+ */
+public class WebdriverExtension implements BeforeEachCallback, AfterEachCallback, HasLogger {
+
+  @Override
+  public void beforeEach(ExtensionContext context) throws Exception {
+    logger().info("beforeEach  -> load default browser webdriver for unit testing");
+    unittestingWebDriverInstance()
+        .ifPresentOrElse(
+            webDriver -> storeWebDriver().accept(context, webDriver),
+            failed -> logger().warning(failed)
+        );
+  }
+
+  @Override
+  public void afterEach(ExtensionContext context) throws Exception {
+    logger().info("afterEach  -> will close and remove the webdriver now..");
+    webdriver()
+        .apply(context)
+        .close();
+    removeWebDriver().accept(context);
+  }
+}
