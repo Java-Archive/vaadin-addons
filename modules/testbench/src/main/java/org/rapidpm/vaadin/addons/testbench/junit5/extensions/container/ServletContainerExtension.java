5c8f6ad27b44867b4a60862e463b7fcf513eab19
Sven Ruppert <sven.ruppert@gmail.com>
2017-12-13 16:10:20 +0100

    ServletContainerExtension for RPM Microservice

--- modules/testbench/src/main/java/org/rapidpm/vaadin/addons/testbench/junit5/extensions/container/ServletContainerExtension.java
+++ modules/testbench/src/main/java/org/rapidpm/vaadin/addons/testbench/junit5/extensions/container/ServletContainerExtension.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.rapidpm.dependencies.core.logger.HasLogger;
-import org.rapidpm.frp.functions.CheckedConsumer;
-import org.rapidpm.frp.functions.CheckedExecutor;
 import org.rapidpm.microservice.Main;
 
 import static org.rapidpm.vaadin.addons.testbench.BrowserDriverFunctions.localeIP;
-import static org.rapidpm.vaadin.addons.testbench.junit5.VaadinPageObject.KEY_VAADIN_SERVER_IP;
+import static org.rapidpm.vaadin.addons.testbench.junit5.pageobject.VaadinPageObject.KEY_VAADIN_SERVER_IP;
 
 /**
  *
