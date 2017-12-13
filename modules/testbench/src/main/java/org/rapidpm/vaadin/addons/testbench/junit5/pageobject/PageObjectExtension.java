55646f0a1f5de323f18fd65de6d55cbba48f8341
Sven Ruppert <sven.ruppert@gmail.com>
2017-12-13 16:11:59 +0100

    PageObject

---
+++ modules/testbench/src/main/java/org/rapidpm/vaadin/addons/testbench/junit5/pageobject/PageObjectExtension.java
@@ -0,0 +1,47 @@
+package org.rapidpm.vaadin.addons.testbench.junit5.pageobject;
+
+import org.junit.jupiter.api.extension.*;
+import org.openqa.selenium.WebDriver;
+import org.rapidpm.dependencies.core.logger.HasLogger;
+import org.rapidpm.vaadin.addons.testbench.junit5.pageobject.AbstractVaadinPageObject;
+import org.rapidpm.vaadin.addons.testbench.junit5.pageobject.PageObject;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import static org.rapidpm.vaadin.addons.testbench.junit5.extensions.ExtensionFunctions.webdriver;
+
+
+/**
+ *
+ */
+public class PageObjectExtension implements ParameterResolver, HasLogger {
+
+  @Override
+  public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+      throws ParameterResolutionException {
+    return parameterContext.getParameter().isAnnotationPresent(PageObject.class);
+  }
+
+  @Override
+  public AbstractVaadinPageObject resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+      throws ParameterResolutionException {
+    logger().info("PageObjectExtension - resolveParameter");
+    final WebDriver webDriver = webdriver().apply(extensionContext);
+    Class<?> pageObject = parameterContext.getParameter().getType();
+    try {
+      Constructor<?> constructor =
+          pageObject.getConstructor(WebDriver.class);
+      return AbstractVaadinPageObject.class.cast(constructor.newInstance(webDriver));
+    } catch (NoSuchMethodException
+        | IllegalAccessException
+        | InstantiationException
+        | InvocationTargetException e) {
+      e.printStackTrace();
+      throw new ParameterResolutionException("was not able to create PageObjectInstance ", e);
+    }
+  }
+
+
+
+}
