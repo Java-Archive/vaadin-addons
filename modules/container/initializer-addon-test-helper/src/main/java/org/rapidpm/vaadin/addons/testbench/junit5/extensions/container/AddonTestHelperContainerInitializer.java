12818e88fbd37aaa9a0619f0490064fad7dbde75
dve <dve@vergien.net>
2018-01-16 14:55:07 +0100

    Add addon-test-helper container initializer

---
+++ modules/container/initializer-addon-test-helper/src/main/java/org/rapidpm/vaadin/addons/testbench/junit5/extensions/container/AddonTestHelperContainerInitializer.java
@@ -0,0 +1,34 @@
+package org.rapidpm.vaadin.addons.testbench.junit5.extensions.container;
+
+import static org.rapidpm.vaadin.addons.framework.NetworkFunctions.localeIP;
+import org.eclipse.jetty.server.Server;
+import org.rapidpm.dependencies.core.logger.HasLogger;
+import org.vaadin.addonhelpers.TServer;
+import com.google.auto.service.AutoService;
+
+@AutoService(ContainerInitializer.class)
+public class AddonTestHelperContainerInitializer implements ContainerInitializer, HasLogger {
+
+  // TODO copy and pasted move to better place
+  public static final String KEY_VAADIN_SERVER_IP = "vaadin.server.ip";
+  public static final String KEY_VAADIN_SERVER_PORT = "vaadin.server.port";
+
+  private Server server;
+
+  @Override
+  public void beforeAll(Class<?> testClass) throws Exception {
+    final String userVaadinServerIP = localeIP().get();
+    logger().info(
+        KEY_VAADIN_SERVER_IP + " ServletContainerExtension - will be -> " + userVaadinServerIP);
+    System.setProperty(KEY_VAADIN_SERVER_IP, userVaadinServerIP);
+
+    System.setProperty(KEY_VAADIN_SERVER_PORT, "9998");
+    TServer tserver = new TServer();
+    server = tserver.startServer();
+  }
+
+  @Override
+  public void afterAll(Class<?> testClass) throws Exception {
+    server.stop();
+  }
+}
