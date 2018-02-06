7ebb6b3e9653c3e0937a81cbe2dadb7690ad943c
dve <dve@vergien.net>
2018-02-06 10:16:37 +0100

    Clean up

--- modules/testbench/src/main/java/org/rapidpm/vaadin/addons/testbench/TestbenchFunctions.java
+++ modules/testbench/src/main/java/org/rapidpm/vaadin/addons/testbench/TestbenchFunctions.java
@@ -2,6 +2,12 @@ package org.rapidpm.vaadin.addons.testbench;
 import static org.rapidpm.frp.matcher.Case.match;
 import static org.rapidpm.frp.matcher.Case.matchCase;
 import static org.rapidpm.frp.model.Result.success;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.rapidpm.frp.model.Result;
 import com.vaadin.testbench.TestBenchDriverProxy;
 import com.vaadin.testbench.elements.ButtonElement;
 import com.vaadin.testbench.elements.TextFieldElement;
@@ -9,13 +15,6 @@ import com.vaadin.testbench.elementsbase.AbstractElement;
 import com.vaadin.ui.AbstractComponent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.TextField;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.rapidpm.frp.model.Result;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
 /**
  *
@@ -32,7 +31,7 @@ public interface TestbenchFunctions {
         ? ((TestBenchDriverProxy) proxyedDriver).getActualDriver()
         : proxyedDriver;
   }
-  
+
   static Function<WebDriver, String> doToString() {
     return driver -> {
       Result<String> result = match(
@@ -46,7 +45,7 @@ public interface TestbenchFunctions {
     return driver.getCapabilities().getBrowserName() + " " + driver.getCapabilities().getVersion()
         + " / " + driver.getCapabilities().getPlatform();
   }
-  
+
   static Function<Class<? extends AbstractComponent>, Optional<Class<? extends AbstractElement>>> conv() {
     return (componentClass) -> {
       final Predicate<Class<? extends AbstractComponent>> is = componentClass::isAssignableFrom;
