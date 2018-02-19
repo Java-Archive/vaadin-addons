2742c7e2e17a3d7e6e4c509da52e9f93a1c92166
dve <dve@vergien.net>
2018-02-19 21:37:07 +0100

    Let test accept value from drone-ci or local unit test

--- modules/testbench/src/test/java/junit/org/rapidpm/vaadin/addons/testbench/BrowserDriverFunctionsTest.java
+++ modules/testbench/src/test/java/junit/org/rapidpm/vaadin/addons/testbench/BrowserDriverFunctionsTest.java
@@ -1,6 +1,6 @@
 package junit.org.rapidpm.vaadin.addons.testbench;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.Properties;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,7 +12,10 @@ public class BrowserDriverFunctionsTest {
   void test001() {
     Properties properties = BrowserDriverFunctions.propertyReader()
         .apply(BrowserDriverFunctions.CONFIG_FOLDER + "config").get();
-    assertEquals("locale", properties.get("unittesting.target"));
+
+    boolean isLocaleOrRapidSelnoid = "locale".equals(properties.get("unittesting.target"))
+        || "selenoid.rapidpm.org".equals(properties.get("unittesting.target"));
+    assertTrue(isLocaleOrRapidSelnoid);
   }
-  
+
 }
